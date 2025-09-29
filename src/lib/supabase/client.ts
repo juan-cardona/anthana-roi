80c4d66185b131c7d3de931a9d05dd6b3bfb0765
juan cardona <joscardona@icloud.com>
2025-09-29 12:41:43 -0600

    fix(login)3

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,9 +1,9 @@
+// src/lib/supabase/client.ts
 "use client";
-import { createClient } from "@supabase/supabase-js";
-
+import { createBrowserClient } from "@supabase/ssr";
 export const supabaseBrowser = () =>
-  createClient(
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { auth: { persistSession: true, autoRefreshToken: true } }
+    { auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true } }
   );
